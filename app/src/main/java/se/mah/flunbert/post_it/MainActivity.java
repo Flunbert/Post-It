955f126665e8e84f91f15606fba9d47986137d51
Unknown <ljpersson16@gmail.com>
2017-02-25 15:14:03 +0100

    -Added complete project to start working on

---
+++ app/src/main/java/se/mah/flunbert/post_it/MainActivity.java
@@ -0,0 +1,13 @@
+package se.mah.flunbert.post_it;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
